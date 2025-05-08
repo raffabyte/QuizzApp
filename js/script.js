d035ef1e586ecb608e0a62d633d17add5d5d713b
raffabyte <raffaelzielniok@gmail.com>
2025-05-08 11:32:57 +0200

    spielbare Version

--- js/script.js
+++ js/script.js
@@ -1,5 +1,3 @@
-
-
 let rightAnswers = 0
 let currentQuestion = 0
 
@@ -8,8 +6,6 @@ function init() {
     showCurrentQuestionNumber()
     showQuestionCard()
 }
-
-
 
 function startQuiz() {
     document.getElementById('startScreen').style = "display : none"
@@ -27,20 +23,27 @@ function showCurrentQuestionNumber() {
 
 function showQuestionCard() {
     if (currentQuestion >= questions.length) {
-        document.getElementById('endScreen').style = ""
-        document.getElementById('questionBody').style = 'display : none'
-        document.getElementById('arrayLengthFinish').innerHTML = questions.length
-        document.getElementById('correctAnswers').innerHTML = rightAnswers
-
+        showEndScreen()
     } else {
-        let question = questions[currentQuestion]
-
-        document.getElementById('questionText').innerHTML = question.question
-        document.getElementById('answer_1').innerHTML = question.answer_1
-        document.getElementById('answer_2').innerHTML = question.answer_2
-        document.getElementById('answer_3').innerHTML = question.answer_3
-        document.getElementById('answer_4').innerHTML = question.answer_4
+        showNextQuestion()
     }
+}
+
+function showNextQuestion() {
+    let question = questions[currentQuestion]
+
+    document.getElementById('questionText').innerHTML = question.question
+    document.getElementById('answer_1').innerHTML = question.answer_1
+    document.getElementById('answer_2').innerHTML = question.answer_2
+    document.getElementById('answer_3').innerHTML = question.answer_3
+    document.getElementById('answer_4').innerHTML = question.answer_4
+}
+
+function showEndScreen() {
+    document.getElementById('endScreen').style = ""
+    document.getElementById('questionBody').style = 'display : none'
+    document.getElementById('arrayLengthFinish').innerHTML = questions.length
+    document.getElementById('correctAnswers').innerHTML = rightAnswers
 }
 
 function answer(answer) {
@@ -56,7 +59,25 @@ function answer(answer) {
         document.getElementById(idOfRightAnswer).parentNode.classList.add('bg-success')
     }
 
+    disableAnswers()
     document.getElementById('next-button').disabled = false
+}
+
+function disableAnswers() {
+    ["answer-1", "answer-2", "answer-3", "answer-4"].forEach(id => {
+        const el = document.getElementById(id);
+        el.classList.add("disabled");
+        el.style.pointerEvents = "none";
+        el.style.opacity = "0.6"; // optional für "deaktivierten Look"
+    });
+}
+
+function enableAnswers() {
+    ["answer-1", "answer-2", "answer-3", "answer-4"].forEach(id => {
+        const el = document.getElementById(id);
+        el.style.pointerEvents = "auto";
+        el.style.opacity = "1";
+    });
 }
 
 function nextQuestion() {
@@ -64,6 +85,7 @@ function nextQuestion() {
     document.getElementById('next-button').disabled = true
     showCurrentQuestionNumber()
     resetAnswers()
+    enableAnswers()
     showQuestionCard()
 }
 
